801d883fe8950c4f30591cde9c796f2cd927e333
SimplyAditya <adityabansal200311@gmail.com>
2026-01-07 12:07:34 +0530

    fix: WhyPartner section has been fixed based on the basic chang

--- src/features/landing/components/WhyPartner.tsx
+++ src/features/landing/components/WhyPartner.tsx
@@ -9,19 +9,19 @@ export const WhyPartner = () => {
             key: 'reach',
             icon: Map,
             iconColor: 'bg-blue-100 text-primary-blue',
-            image: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=2070&auto=format&fit=crop' // Placeholder map/location image
+            image: 'https://lh3.googleusercontent.com/aida-public/AB6AXuD-BNcx3Zl7GjaS37x-W28HKYrXed_pTVxn6QAaoltZKuf8u4Kuz7zy1RtIHNZp45_LjemawhdcyKXN_4DXUNV53VHWC7799YeQYdoXEpPzBGtD2tE6SeH1CcZrgubwadh580zVPhqesV-JB7MKvdUlcrQ2mUaqKP4TX5TMyOgwDOsg1g8MRiyQGjDESqgv1TRl5hNElF_3NK8oOPzMUCrmOEzN8fFncSUh2skbEyhCPqsc-zOJPizf3ZXQsU5PM1sYLRBNbYJDRHU' // Placeholder map/location image
         },
         {
             key: 'delivery',
             icon: Truck,
             iconColor: 'bg-orange-100 text-secondary-orange',
-            image: 'https://images.unsplash.com/photo-1613346945084-35db52d7b4a3?q=80&w=2070&auto=format&fit=crop' // Placeholder delivery image
+            image: '/ustart_delivery.png' // Placeholder delivery image
         },
         {
             key: 'support',
             icon: Headset,
             iconColor: 'bg-green-100 text-green-600',
-            image: 'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=2072&auto=format&fit=crop' // Placeholder support image
+            image: 'https://lh3.googleusercontent.com/aida-public/AB6AXuC4om3KC9SI_s2sI3zU02UrkVE2frvtrwSoAN19sRkaUtIOwajsQG_om8dzTHWXrbByAf-CIgVLPi7jDfPm7ROukAy4xp0L4AODghw14iv64FTa-xIYPzbF6fdkeRaHFqVnCCodHpEWCKL2dRotGbKBEartb6XJKb8-uNqDETnisEQe4g7tN3R4Zu3CcbmiwLlYvm4tzG9CSjZ4Zrf9MKNqlTZwGEhJQHK1lJsqWUEss4DDzG8lxI-KI09UHXW4X1-r2MmEpwsgRas' // Placeholder support image
         }
     ];
 
@@ -52,11 +52,11 @@ export const WhyPartner = () => {
                                 {t(`landing.whyPartner.cards.${feature.key}.description`)}
                             </p>
 
-                            <div className="rounded-2xl overflow-hidden h-48 w-full bg-gray-100">
+                            <div className="rounded-2xl overflow-hidden w-full bg-gray-100 aspect-square">
                                 <img
                                     src={feature.image}
                                     alt={t(`landing.whyPartner.cards.${feature.key}.title`)}
-                                    className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-500"
+                                    className="w-full h-full object-cover transform transition-transform duration-500"
                                 />
                             </div>
                         </div>
