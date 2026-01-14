d43500035d986b858adf9b1bb8081c27b49aafd0
SimplyAditya <adityabansal200311@gmail.com>
2026-01-14 14:08:54 +0530

    fix: layout

--- src/features/dashboard/layouts/DashboardLayout.tsx
+++ src/features/dashboard/layouts/DashboardLayout.tsx
@@ -30,7 +30,7 @@ export const DashboardLayout = () => {
                 onLogoutClick={() => setShowLogoutConfirm(true)}
             />
 
-            <div className="flex-1 flex flex-col min-h-screen min-w-0 w-screen">
+            <div className="flex-1 flex flex-col min-h-screen min-w-0">
                 {/* Fixed/Sticky Header */}
                 <DashboardHeader
                     user={user || { name: 'Partner' }}
