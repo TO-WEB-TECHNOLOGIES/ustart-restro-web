f7398aad18811519f9a7da0163c4deaff3539034
SimplyAditya <adityabansal200311@gmail.com>
2026-02-16 22:41:39 +0530

    chore: Update API base URL from localhost to UAT environment.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // TODO: Replace with environment variable later
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://uat.ustart.in';
 
 export const api = axios.create({
     baseURL: BASE_URL,
